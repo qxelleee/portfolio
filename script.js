98067f53491d8e05130c738ca4b42098c5ac3017
qxelleee <dhritimankashyap20@gmail.com>
2025-07-23 15:24:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector('nav');
+const dropdown = document.querySelector('.dropdown');
+
+function menu(v) {
+  if (v == 'ab') {
+    dropdown.style.display = 'flex'
+  }
+  else {
+    dropdown.style.display = 'none'
+ }
+}
+
+const stl= getComputedStyle(dropdown);
+console.log(dropdown.getBoundingClientRect().height)
+/*const styleroots = document.querySelector(':root');
+styleroots.style.setProperty('--default', '#051429');
+styleroots.style.setProperty('--textcolor', 'white');
+styleroots.style.setProperty('--headerbg', 'url(/img/bgdark.jpg)');*/
